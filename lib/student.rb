c08e4e0e9384de0369d83b1ab0e1e489d0edc6d7
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-19 18:52:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student
 
   attr_reader :id
-  attr_accessor :name, :grade 
+  attr_accessor :name, :grade
 
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
     @id = id
   end
-  
+
 end
